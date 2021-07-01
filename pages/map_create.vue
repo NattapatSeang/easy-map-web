004e27576060d6ed4c287816b37040a1f5ea7b0e
NattapatSeang <nattapat_1998@hotmail.com>
2021-07-01 19:50:10 +0700

    Create

---
+++ pages/map_create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="con-center">
+    <h1>Hello {{ name }}</h1>
+    <input type="text" :value="name"/>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Artdvp"
+    }
+  }
+}
+</script>
+<style>
+.con-center {
+  text-align: center;
+  margin-top: 1rem;
+}
+</style>
---
+++ pages/your_map.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Fetch Data with API</h1>
+    </div>
+</template>
+<script>
+import EmployeeList from './components/map_list.vue'
+export default {
+    name: 'App'
+    components: {
+        map_list
+    }
+}
+</script>
+<style>
+    #app {
+        
+    }
+</style>
